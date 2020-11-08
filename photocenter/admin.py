f3858ece5509dfc3a26bbc2c3859c38aebb56ded
MrFlava <thatelitemaili33t@gmail.com>
2020-11-08 21:29:06 +0200

    Added all models

--- photocenter/admin.py
+++ photocenter/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import PhotoCenter, PhotoOffice, PhotoStand
+from .models import (PhotoCenter, PhotoOffice, PhotoStand, Order, Item, Provider,
+                     ItemProvider, AdditionalService,  ItemOrder, AdditionalServicesOrder)
 
 # Register your models here.
 
@@ -17,6 +18,42 @@ class PhotoStandAdmin(admin.ModelAdmin):
     model = PhotoStand
 
 
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+
+
+class ItemAdmin(admin.ModelAdmin):
+    model = Item
+
+
+class ProviderAdmin(admin.ModelAdmin):
+    model = Provider
+
+
+class ItemProviderAdmin(admin.ModelAdmin):
+    model = ItemProvider
+
+
+class AdditionalServiceAdmin(admin.ModelAdmin):
+    model = AdditionalService
+
+
+class ItemOrderAdmin(admin.ModelAdmin):
+    model = ItemOrder
+
+
+class AdditionalServicesOrderAdmin(admin.ModelAdmin):
+    model = AdditionalServicesOrder
+
+
+admin.site.register(AdditionalServicesOrder, AdditionalServicesOrderAdmin)
+admin.site.register(AdditionalService, AdditionalServiceAdmin)
+admin.site.register(ItemProvider, ItemProviderAdmin)
 admin.site.register(PhotoCenter, PhotoCenterAdmin)
 admin.site.register(PhotoOffice, PhotoOfficeAdmin)
 admin.site.register(PhotoStand, PhotoStandAdmin)
+admin.site.register(ItemOrder, ItemOrderAdmin)
+admin.site.register(Provider, ProviderAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Item, ItemAdmin)
+
--- photocenter/models.py
+++ photocenter/models.py
@@ -1,23 +1,99 @@
 import enum
+import datetime
 
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth import get_user_model
+
 
 
 # Create your models here.
 
-# class OrderType(enum.Enum):
-#     FILM_DEVELOPMENT = 'film development'
-#     PHOTO_PRINTING_AND_DEVELOPMENT = 'Photo printing and development'
-#     PRINT_TOGETHER = 'print together'
+class AdditionalServicesType(enum.Enum):
+    PASSPORT_PHOTO = 'Passport photo'
+    PHOTO_RESTORATION = 'Photo restoration'
+    CAMERA_RENTAL = 'Camera rental'
+    ART_PHOTO = 'Art photo'
+    PROFESSIONAL_PHOTOGRAPHER = 'Professional photographer'
 
-# class PaperType(enum.Enum):
-#     MATTE = 'Matte'
-#     GLOSSY = 'Glossy'
-#     SEMI_GLOSSY = 'Semi-Glossy'
-#     SUPERGLOSSY = 'SuperGlossy'
-#     SILK = 'Silk'
-#     SATIN = 'Satin'
+    @classmethod
+    def choices(cls):
+        print(tuple((i.name, i.value) for i in cls))
+        return tuple((i.name, i.value) for i in cls)
+
+
+class ItemType(enum.Enum):
+    CAMERA = 'Camera'
+    LENSES = 'Lenses'
+    OUTBREAKS = 'Outbreaks'
+    PHOTO_PAPER = 'Photo paper'
+    CAMERA_ROLL = 'Camera roll'
+    PHOTOGRAPHIC_REAGENTS = 'Photographic reagents'
+    ALBUM = 'Album'
+
+    @classmethod
+    def choices(cls):
+        print(tuple((i.name, i.value) for i in cls))
+        return tuple((i.name, i.value) for i in cls)
+
+
+class CustomerType(enum.Enum):
+    PROFESSIONAL = 'professional'
+    FAN = 'FAN'
+
+    @classmethod
+    def choices(cls):
+        print(tuple((i.name, i.value) for i in cls))
+        return tuple((i.name, i.value) for i in cls)
+
+
+class OrderType(enum.Enum):
+    FILM_DEVELOPMENT = 'film development'
+    PHOTO_PRINTING_AND_DEVELOPMENT = 'Photo printing and development'
+    PRINT_TOGETHER = 'print together'
+
+
+class OrderPriorityType(enum.Enum):
+    URGENT_PRIORITY = 'Urgent priority'
+    ORDINARY_PRIORITY = 'Ordinary priority'
+
+    @classmethod
+    def choices(cls):
+        print(tuple((i.name, i.value) for i in cls))
+        return tuple((i.name, i.value) for i in cls)
+
+
+class PaperFormat(enum.Enum):
+    A0 = 'A0'
+    A1 = 'A1'
+    A2 = 'A2'
+    A3 = 'A3'
+    A4 = 'A4'
+    A5 = 'A5'
+    A6 = 'A6'
+    A7 = 'A7'
+    A8 = 'A8'
+    A9 = 'A9'
+    A10 = 'A10'
+
+    @classmethod
+    def choices(cls):
+        print(tuple((i.name, i.value) for i in cls))
+        return tuple((i.name, i.value) for i in cls)
+
+
+class PaperType(enum.Enum):
+    MATTE = 'Matte'
+    GLOSSY = 'Glossy'
+    SEMI_GLOSSY = 'Semi-Glossy'
+    SUPERGLOSSY = 'SuperGlossy'
+    SILK = 'Silk'
+    SATIN = 'Satin'
+
+    @classmethod
+    def choices(cls):
+        print(tuple((i.name, i.value) for i in cls))
+        return tuple((i.name, i.value) for i in cls)
 
 
 class PhotoCenter(models.Model):
@@ -62,8 +138,82 @@ class PhotoStand(models.Model):
         return f"Stand #{self.pk} located at {self.address}"
 
 
-# class Order(models.Model):
-#
-# class Item(models.Model):
-#
-# class Customer(models.Model):
+class Customer(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    type = models.CharField(max_length=120, choices=CustomerType.choices())
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f"Customer #{self.pk}: {self.user.name}"
+
+
+class Order(models.Model):
+    photos_from_each_frame_quantity = models.IntegerField(default=1, validators=[MinValueValidator(1)])
+    photos_quantity = models.IntegerField(default=1, validators=[MinValueValidator(1)])
+    paper_format = models.CharField(max_length=120, choices=PaperFormat.choices())
+    paper_type = models.CharField(max_length=120, choices=PaperType.choices())
+    priority = models.CharField(max_length=120, choices=OrderPriorityType.choices())
+    term = models.DateTimeField(default=datetime.datetime.now())
+    photo_office = models.ForeignKey(PhotoOffice, on_delete=models.CASCADE, null=True, blank=True)
+    photo_stand = models.ForeignKey(PhotoStand, on_delete=models.CASCADE, null=True, blank=True)
+    price = models.PositiveIntegerField(default=0)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f"Order #{self.pk}for {self.customer}"
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=120)
+    items = models.ManyToManyField('Item', through='ItemProvider')
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f"Provider #{self.pk}: {self.name}"
+
+
+class Item(models.Model):
+    item_type = models.CharField(max_length=120, choices=ItemType.choices())
+    title = models.CharField(max_length=120)
+    description = models.TextField(max_length=250)
+    price = models.PositiveIntegerField(default=0)
+    providers = models.ManyToManyField('Provider', through='ItemProvider')
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f"Item #{self.pk}: {self.item_type}, {self.title}"
+
+
+class ItemProvider(models.Model):
+    provider = models.ForeignKey(Provider, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+
+class AdditionalService(models.Model):
+    service_type = models.CharField(max_length=120, choices=AdditionalServicesType.choices())
+    photo_office = models.ForeignKey(PhotoOffice, on_delete=models.CASCADE)
+
+
+class ItemOrder(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f"Order #{self.pk}for  {self.customer}"
+
+
+class AdditionalServicesOrder(models.Model):
+    additional_service = models.ForeignKey(AdditionalService, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return f"Order #{self.pk}for  {self.customer}"
